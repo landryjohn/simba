e9556a5762b241c8ebf9df5f8a48d74615952925
Landry John MELI <landryjohnmeli@gmail.com>
2021-12-30 15:11:00 +0100

    configure database connection | create template and static folder | create users admin and simba_api | create blueprint template and extend index.html on app.html | add statics files

--- simba_project/simba_app/views.py
+++ simba_project/simba_app/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request): 
-    return HttpResponse("Simba is here")
+    return render(request, 'simba_app/index.html')
