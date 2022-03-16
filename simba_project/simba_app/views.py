d10b387cf9a074314f2c8ef53e56e40938ff01f7
landry john <landryjohnmeli@gmail.com>
2022-03-16 04:28:17 +0000

    Added get_intrusion and send intrusions report logic. Added telegram notification

--- simba_project/simba_app/views.py
+++ simba_project/simba_app/views.py
@@ -1,4 +1,6 @@
+import datetime
 import json
+import re
 import subprocess, requests
 from django.http import HttpResponse
 from django.contrib.auth.backends import UserModel
@@ -18,14 +20,27 @@ from simba_app.scripts.scripts import shell_command, SCRIPT_PATH, SUPPORTED_METH
 
 from random import choice
 
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+
+# TODO : remove the test view 
 def test(request):
-    AI_SCRIPT_PATH = "/home/mdiai/Desktop/simba/simba_project/simba_app/scripts/ai.py"
-    with open('/home/mdiai/Desktop/simba/simba_project/simba_app/scripts/command.txt', 'w') as command_file:
-        command_file.write("rapport d'intrusions de la journée")
-    output = subprocess.run(f"python3 {AI_SCRIPT_PATH}".split(), capture_output=True, text=True).stdout
-    print('********res')
-    print(output)
-    return HttpResponse(output)
+    BASE_URL = "http://192.168.8.30:9000"
+    data = {}
+    data['result'] = requests.post(f"{BASE_URL}/api/system_call/", data={'method':'get_intrusion_report'}).json()['message']
+    alerts_number = data['result'].count('Classification')
+    if alerts_number : 
+        today = f"{datetime.datetime.now():%d-%m-%Y}"
+        flow_list = re.findall('.* ->', data['result'])
+        print('flow list', flow_list)
+        attackers = set([ machines.split()[-2] for machines in flow_list ])
+        print('attackers', attackers)
+        data['bot_answer'] = f"Je détecte actuellement {alerts_number} alertes dans le système de détection des intrusions pour la journée du {today.replace('-',' ')}. "
+        data['bot_answer'] += f"Les machines suspectent dans le réseau 192.168.8.0/24 sont les suivantes : "
+        data['bot_answer'] += f"{' '.join(attackers)}"
+    return HttpResponse(data['bot_answer'])
 
 def index(request): 
     if request.user.is_authenticated :
@@ -74,7 +89,6 @@ def prediction(request):
     # intent = brain.get_intent(intents, brain.data)
     output = subprocess.run(f"python3 {AI_SCRIPT_PATH}".split(), capture_output=True, text=True).stdout
     intent = dict(json.loads(output))
-    print(intent)
     data = {}
     data['bot_answer'] = choice(intent["responses"])
     data['tag'] = intent["tag"] 
@@ -83,19 +97,39 @@ def prediction(request):
     elif intent["tag"] == 'services_status' :
         # show_services_status
         data['result'] = requests.post(f"{BASE_URL}/api/system_call/", data={'method':'show_services_status'}).json()['message'].replace('\n', '<br>')
+        data['bot_answer'] = "Voici, le statut de fonctionnement des services dans le serveur de détection d'intrusions" 
     elif intent["tag"] == 'signature_database' : 
         # get_signature_database
         data['result'] = requests.post(f"{BASE_URL}/api/system_call/", data={'method':'get_signature_database'}).json()['message'].replace('\n', '<br>')
+        rules_number = data['result'].count('<br>') 
+        data['bot_answer'] = f"Il y a actuellement {rules_number} profiles dans la base des signatures d'attaques"
     elif intent["tag"] == 'simba_rules' :
         # show_simba_rules
         data['result'] = requests.post(f"{BASE_URL}/api/system_call/", data={'method':'show_simba_rules'}).json()['message'].replace('\n', '<br>')
+        data['bot_answer'] = "Voici les règles personnalisées de MDAIA"
     elif intent["tag"] == 'intrusion_report' : 
         # get_intrusion_report
         data['result'] = requests.post(f"{BASE_URL}/api/system_call/", data={'method':'get_intrusion_report'}).json()['message'].replace('\n', '<br>')
+        alerts_number = data['result'].count('Classification')
+        if alerts_number : 
+            today = f"{datetime.datetime.now():%d-%m-%Y}"
+            flow_list = re.findall('.* ->', data['result'])
+            attackers = set([ machines.split()[-2] for machines in flow_list ])
+            data['bot_answer'] = f"Je détecte actuellement {alerts_number} alertes dans le système de détection des intrusions pour la journée du {today.replace('-',' ')}. "
+            data['bot_answer'] += f"Les machines suspectent dans le réseau 192.168.8.0/24 sont les suivantes : "
+            data['bot_answer'] += f"{' '.join(attackers)}"
+        else : 
+            data['bot_answer'] = "Aucune alerte à signaler pour aujourd'hui"
     elif intent["tag"] == 'send_intrusion_report' : 
-        # TODO : add logic according to the intent
-        # get_intrusion_report 
-        data['result'] = "Envoi du rapport d'intrusion"
+        # get_intrusion_report via telegram
+        BOT_TOKEN = env('TELEGRAM_BOT_KEY')
+        CHATID = env('TELEGRAM_CHAT_ID')
+        content = requests.post(f"{BASE_URL}/api/system_call/", data={'method':'get_intrusion_report'}).json()['message']
+        r = requests.post(f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHATID}&text=Hello World!')
+        if r["ok"] :
+            data['bot_answer'] = "Le rapport d'intrusions à été bien envoyé"
+        else :
+            data['bot_answer'] = "un problème est survenu lors de l'envoi du rapport d'intrusion. Consulter les logs."
     elif intent["tag"] == 'block_user_rule' : 
         # TODO : add logic according to the intent
         data['result'] = "Bloquer un utilisateur"
